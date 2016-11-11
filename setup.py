ccdbe7a9d2dc899726cde232edbc7fee880965ac
hagne <hagen@hagne.net>
2016-11-11 13:23:34 -0700

    new: setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='hysplit-py',
+    version='',
+    packages=['hysplit_py'],
+    url='',
+    license='',
+    author='Hagen Telg',
+    author_email='hagen@hagnet.net',
+    description=''
+)
